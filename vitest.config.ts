17a1e568551d727600a2d8c3f1690541361b6705
seiya <20365512+seiyab@users.noreply.github.com>
2025-07-22 03:54:37 +0000

    prettier configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,5 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    exclude: ['node_modules/**', 'dist/**', 'references/**'],
-  },
-})
+	test: { exclude: ["node_modules/**", "dist/**", "references/**"] },
+});
