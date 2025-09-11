52c89a1d59df02f160c7f6b3efc2b16ed550b391
seiya <20365512+seiyab@users.noreply.github.com>
2025-09-11 13:01:39 +0000

    loop

--- tests/snapshot.test.ts
+++ tests/snapshot.test.ts
@@ -18,6 +18,7 @@ const files = [
 	"import-between-modules/mod2.wat",
 	"vector-count-value/vcount.wat",
 	"locals/locals.wat",
+	"loops/loops.wat",
 ];
 
 const root = path.join(import.meta.dirname, "..");
